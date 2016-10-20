a1ded922e9f088b018b1247041d56a9d9eebd204
fernandofmendes <fernando.mendes@webca.com.br>
2016-10-20 17:24:41 -0200

    Revert "traducao de busca"
    This reverts commit 12c3f92bdeb92e1c20bc9516b5742508d4d00618.

--- public/wp-content/themes/madeleine/searchform.php
+++ public/wp-content/themes/madeleine/searchform.php
@@ -11,12 +11,12 @@
         <label>
             <span class="screen-reader-text"><?php echo _x('Search for:', 'label', 'madeleine') ?></span>
             <input id="s" type="search" class="search-form__field"
-                   placeholder="<?php echo esc_attr_x('Digite sua busca ', 'placeholder', 'madeleine') ?>"
+                   placeholder="<?php echo esc_attr_x('Enter keyword ', 'placeholder', 'madeleine') ?>"
                    value="<?php echo get_search_query() ?>" name="s"
                    title="<?php echo esc_attr_x('Search for:', 'label', 'madeleine') ?>"/>
         </label>
     </div>
     <div class="search-form_btn_wr">
-        <button type="submit" class="search-form__submit btn"><i class="material-icons">Buscar</i></button>
+        <button type="submit" class="search-form__submit btn"><i class="material-icons">search</i></button>
     </div>
 </form>
